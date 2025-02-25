075c4f6d924a2e1137c3f9a2d33b81aa51b323a5
jpswag <johnpan@umich.edu>
2025-02-25 10:07:58 -0500

    working_on_mouse_move

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Import Three.js and OrbitControls
-import * as THREE from './node_modules/three/build/three.module.js';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.136.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.136.0/examples/jsm/controls/OrbitControls.js';
+
 
 
 
