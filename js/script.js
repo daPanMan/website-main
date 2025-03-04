c669f43c71c28c4a3e17b5daf36a9a3be6d2b039
dapanman <galvatronus2001@gmail.com>
2025-03-03 19:38:33 -0500

    fixing_bgm_play

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ document.getElementById("enter-button").addEventListener("click", () => {
         bgm.play();
         threeCanvas.style.display = "block"; // Show Three.js canvas
         cssRenderer.domElement.style.display = "block"; // Show CSS3DRenderer
-        gsap.to(threeCanvas, { opacity: 1, duration: 1, ease: "power2.out" }); // Smooth fade-in
+        gsap.to(threeCanvas, { opacity: 1, duration: 2, ease: "power2.out" }); // Smooth fade-in
     }, 500);
 });
 
